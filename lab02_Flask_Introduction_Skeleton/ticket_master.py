d4e8bf0b0c3f371a39715642f00050e8d0d46dc5
Sean-ORourke <hellohinope@gmail.com>
2024-11-24 16:11:17 -0500

    replacing remaining links with friendly ones

--- lab02_Flask_Introduction_Skeleton/ticket_master.py
+++ lab02_Flask_Introduction_Skeleton/ticket_master.py
@@ -6,36 +6,19 @@ app = Flask(__name__)
 def default():
     return render_template("main_page.html", reference_to_details_page = url_for("christmas_story"))
 
+
+# https://youtu.be/pfpWasQvBfg?si=A9NU5JXpBOFVPdBF&t=37 "you do not need to have the functionality for the other 5, just pay attetion on this one here"
 @app.route("/christmas_story/")
-def christmas_story():
+def christmas_story(): 
     return render_template("christmas_story.html", reference_to_buying_page = url_for("buy_tickets", play_name = "christmas_story"))
 
 @app.route("/buy_tickets/<play_name>", methods=["post"])
 def buy_tickets(play_name):
     play_name = str(play_name)
-    return render_template("buy_tickets.html", play_name=play_name, refrence_to_transaction_page = url_for("transaction_page", play_name=play_name)) # your code here
+    return render_template("buy_tickets.html", play_name=play_name, refrence_to_transaction_page = url_for("transaction_page", play_name=play_name))
 
 @app.route("/transaction_page/<play_name>", methods=["post"])
 def transaction_page(play_name):
-
-
-    # order_items_in_String_format = request.form["transaction_page"].strip()
-    # order_items_in_array_format = order_items_in_String_format.split("\n")
-    # customer = request.form["transaction_page"]
-    # global order_number
-    # order_number = order_number + 1
-
-    
-
-    # #  Gather form data into a JSON object
-    # const formData = {
-    #     zone: document.getElementById("zone").value,
-    #     sector: document.getElementById("sector").value,
-    #     customer_name: document.getElementById("customer_name").value,
-    #     date_and_time: document.getElementById("date_and_time").value,
-    # }
-
-    # data = request.get_json()
     
     # # Extract individual fields
     zone = request.form["zone"]
@@ -50,7 +33,7 @@ def transaction_page(play_name):
         "date_and_time": date_and_time
     }
 
-    return render_template("transaction_page.html", play_name=play_name, transaction = current_transaction, reference_to_main_page = url_for("default")) # your code here
+    return render_template("transaction_page.html", play_name=play_name, transaction = current_transaction, reference_to_main_page = url_for("default"))
 
 if __name__ == "__main__":
     app.run(debug=True)
